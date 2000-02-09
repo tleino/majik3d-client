fb1c75575ce7fcb6210a562c1ac098d56a0aef4f
Tommi Leino <namhas@majik.netti.fi>
2000-02-09 00:17:52 +0000

    glClear in login screen is needed.

--- src/mcOverlay.cpp
+++ src/mcOverlay.cpp
@@ -109,6 +109,8 @@ Overlay::draw()
       // calculate fps
       status_text->setLabel(fps_text);
     }
+  else
+    glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 
   glEnable (GL_BLEND);
   glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA);
